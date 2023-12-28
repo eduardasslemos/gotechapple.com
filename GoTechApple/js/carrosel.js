a662d65893bb8014ae1cfa516c39962d9f9e5f4a
eduardasslemos <155028310+eduardasslemos@users.noreply.github.com>
2023-12-28 15:02:38 -0300

    Add files via upload

---
+++ GoTechApple/js/carrosel.js
@@ -0,0 +1,32 @@
+/*const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img")
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length- 1){
+        idx = 0;
+    }
+
+    imgs.style.transform = 'translateX(${-idx * 500}px)';
+}
+
+setInterval(carrossel, 1800)*/
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+
+}, 2000)
+
+function nextImage(){
+    count++;
+    if(count > 4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
